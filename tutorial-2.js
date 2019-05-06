e49c9fbf782fa45bfaa72ac049144c8207724bdb
Anderson Santos <andersonc1981@gmail.com>
2019-05-06 10:48:21 -0300

    Update tutorial-2.js

--- tutorial-2.js
+++ tutorial-2.js
@@ -3,7 +3,8 @@ const { WebClient } = require('@slack/web-api');
 // An access token (from your Slack app or custom integration - xoxp, xoxb)
 //const token = process.env.SLACK_TOKEN;
 
-const web = new WebClient('xoxp-318392221105-318942821602-626453242320-bacc497c229b8041d987510a6b7c0048');
+//xoxp .....
+const web = new WebClient('your_token_here');
 
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
 const conversationId = 'C9E03H9L7';
